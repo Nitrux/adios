262237a419bc017ce55783e47dcdfa041352b4e7
Uri Herrera <hello@uriherrera.com>
2025-12-30 12:13:52 -0600

    create pkg

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# adios
+# ADIOS
 Adaptive Deadline I/O Scheduler
+
+# Issues
+If you find problems with the contents of this repository please create an issue.
+
+©2025 Nitrux Latinoamericana S.C.
---
+++ scripts/install-build-deps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2025 <Nitrux Latinoamericana S.C. <hello@nxos.org>>
+
+
+# -- Exit on errors.
+
+set -e
+
+
+# -- Check if running as root.
+
+if [ "$EUID" -ne 0 ]; then
+    APT_COMMAND="sudo apt"
+else
+    APT_COMMAND="apt"
+fi
+
+
+# -- Install build packages.
+
+$APT_COMMAND update -q
+$APT_COMMAND install -y --no-install-recommends \
+    dpkg-dev \
+    git \
+    file
